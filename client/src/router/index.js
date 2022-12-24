8fe570a7edc84735d2b9d45173989f420374b9fb
Claudia <clodos_m92@hotmail.com>
2022-12-24 15:05:50 +0100

    Weitere Views mit Routen, Edit User Account Form

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,20 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import UserCockpitOverview from "@/views/UserCockpitOverview";
 import AnswerNew from "@/views/AnswerNew";
 import QuestionNew from "@/views/QuestionNew";
+import UserCockpitEdit from "@/views/UserCockpitEdit";
+import AdminCockpitOverview from "@/views/AdminCockpitOverview";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: Home },
-        { path: '/login', component: Login },
-        { path: '/register', component: Register },
-        { path: '/user/cockpit/overview', component: UserCockpitOverview },
-        { path: '/question/new', component: QuestionNew },
-        { path: '/answer/new', component: AnswerNew}
+        {path: '/', component: Home},
+        {path: '/login', component: Login},
+        {path: '/register', component: Register},
+        {path: '/user/cockpit/overview', component: UserCockpitOverview},
+        {path: '/question/new', component: QuestionNew},
+        {path: '/answer/new', component: AnswerNew},
+        {path: '/user/cockpit/edit', component: UserCockpitEdit},
+        {path: '/admin/cockpit/overview', component: AdminCockpitOverview}
     ]
 })
 
---
+++ client/src/views/AdminCockpitOverview.vue
@@ -0,0 +1,15 @@
+<template>
+<main class="adminCockpitOverview-page">
+  <h3>Admin Cockpit</h3>
+</main>
+</template>
+
+<script>
+export default {
+  name: "AdminCockpitOverview"
+}
+</script>
+
+<style scoped>
+
+</style>
