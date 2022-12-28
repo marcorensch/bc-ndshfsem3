3aa975cf7496c425945b2eb898a884e740eaaec1
Marco Rensch <marco.rensch@nx-designs.ch>
2022-12-28 12:49:21 +0100

    https implemented

--- server/server.mjs
+++ server/server.mjs
@@ -1,30 +1,32 @@
 import * as dotenv  from 'dotenv';
 dotenv.config();
+import * as https from "https";
 import express from 'express';
 import 'babel-polyfill';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import usersRoute from './routes/users.mjs';
 import questionsRoute from './routes/questions.mjs';
 import authRoute from './routes/auth.mjs';
 import categoriesRoute from './routes/categories.mjs';
-import mariadb from 'mariadb';
+import * as fs from "fs";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 let corsOptions = {
     origin: '*',
-
     optionsSuccessStatus: 200 ,// some legacy browsers (IE11, various SmartTVs) choke on 204
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
-
-
 }
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app).listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
 
 app.use(cors( corsOptions ));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -43,8 +45,4 @@ app.use((req, res, next) => {
     err.status = 404;
     res.send('Route not found');
     next(err);
-});
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
 });
