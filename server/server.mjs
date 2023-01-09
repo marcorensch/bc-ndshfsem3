7064a74410f3f9dae7f8d03dd24d0c80053f5d6b
Marco <marco.rensch@area59.ch>
2023-01-09 07:50:21 +0100

    Fixed Wrong env key for Server Port

--- server/server.mjs
+++ server/server.mjs
@@ -15,7 +15,7 @@ import QuestionHelper from "./helper/QuestionHelper.mjs";
 import chalk from "chalk";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 let corsOptions = {
     origin: "*",
